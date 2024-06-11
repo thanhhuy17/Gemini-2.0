0fe5b7f0f6ab1d9fde2c66f7b2dcee64305a061b
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-06-11 16:08:53 +0700

    fisnish UI

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,9 @@
 // import React from "react";
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Main.css";
 const Main = () => {
+  const[extended, setExtended] = useState(false)
   return (
     <div className="main">
       <div className="nav">
@@ -36,6 +38,21 @@ const Main = () => {
           <div className="card">
             <p>Improve the readability of the following code</p>
             <img src={assets.code_icon} alt="image" />
+          </div>
+
+          <div className="main__bottom">
+            <div className="search__box">
+              <input type="text" placeholder="Enter a prompt here" />
+              <div className="search__box-icon">
+                <img className="search__img-icon" src={assets.gallery_icon} alt="" />
+                <img className="search__img-icon" src={assets.mic_icon} alt="" />
+                {extended?<img className="search__img-icon" src={assets.send_icon} alt="" />:null}
+              </div>
+            </div>
+            <div className="main__botton-descrip">
+              <p>Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
+            </div>
+
           </div>
         </div>
       </div>
