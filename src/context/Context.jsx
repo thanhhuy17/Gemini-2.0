8dfc6443e3bfc71291284affc96fd402d5251354
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-06-12 16:25:26 +0700

    update ai

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,4 +1,25 @@
-import { createContext } from "react";
 
-export const Context = createContext();
+// import { ChatSession } from "@google/generative-ai";
+// import { createContext } from "react";
 
+// export const Context = createContext();
+
+// const ContextPrivider = (props)=>{
+
+//     const onSend = async(prompt)=>{
+//         await ChatSession(prompt)
+//     }
+
+//     onSend("what is react js?")
+
+//     const contextValue = {
+
+//     }
+
+//     return (
+//         <Context.Provider value={contextValue}>
+//             {props.children}
+//         </Context.Provider>
+//     )
+// }
+// export default ContextPrivider
